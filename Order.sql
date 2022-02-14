e9d22463139f1a609d2bd59335a73ab010520446
ItsJosephVaughn <99619621+ItsJosephVaughn@users.noreply.github.com>
2022-02-14 16:43:25 -0500

    Create Order.sql

---
+++ Order.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Order (
+  OrderID     SMALLINT UNSIGNED AUTO_INCREMENT,
+  Name          VARCHAR(50),
+  Description   VARCHAR(255),
+  Price         DECIMAL(2,2),
+  PRIMARY KEY (ID)
+);
