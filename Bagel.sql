6d5a18e74b6b63cd3146d9784627c18386536be1
ItsJosephVaughn <99619621+ItsJosephVaughn@users.noreply.github.com>
2022-02-14 16:40:29 -0500

    Update Bagel.sql

--- Bagel.sql
+++ Bagel.sql
@@ -1,1 +1,7 @@
 CREATE TABLE Bagel (
+  ID            SMALLINT UNSIGNED AUTO_INCREMENT,
+  Name          VARCHAR(50),
+  Description   VARCHAR(255),
+  Price         DECIMAL(2,2),
+  PRIMARY KEY (ID)
+);
