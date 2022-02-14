5cbbc05faba18453a98ec12107fdb6107dca048c
ItsJosephVaughn <99619621+ItsJosephVaughn@users.noreply.github.com>
2022-02-14 17:00:31 -0500

    Update Customer.sql

--- Customer.sql
+++ Customer.sql
@@ -1,1 +1,13 @@
-
+CREATE TABLE Customer
+(
+  ID INT UNSIGNED,
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Address1 VARCHAR(50),
+  Address2 VARCHAR(50),
+  City VARCHAR(50),
+  State CHAR(2),
+  Zip CHAR(6),
+  MobilePhone CHAR(10),
+  PRIMARY KEY (ID),
+);
